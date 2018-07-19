02b6404519e1d3c9a0d472adc62b9708b7399cd5
Arunas Cesonis <karunce@gmail.com>
2018-07-19 10:14:19 +0100

    Working WebSocket

--- src/Main.purs
+++ src/Main.purs
@@ -4,6 +4,20 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+import Web.Socket.WebSocket as WS
+import Web.Socket.Event.EventTypes as WSET
+import Web.Event.EventTarget as EET
+
+-- conn <- WS.create "ws://echo.websocket.org" []
+
 main :: Effect Unit
 main = do
-  log "Hello sailor!"
+  socket <- WS.create "ws://localhost:7080" []
+  listener <- EET.eventListener \ev -> do
+    log "123"
+  openListener <- EET.eventListener \ev -> do
+    log "open"
+    WS.sendString socket "HELLO"
+  EET.addEventListener WSET.onMessage listener false (WS.toEventTarget socket)
+  EET.addEventListener WSET.onOpen openListener false (WS.toEventTarget socket)
+  log "end of main"
