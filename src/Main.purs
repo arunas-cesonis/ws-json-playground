a986b5363299c3cec485750f109ffabec293a3a0
Arunas Cesonis <karunce@gmail.com>
2018-07-19 10:29:09 +0100

    WebSocket sends JSON

--- src/Main.purs
+++ src/Main.purs
@@ -8,7 +8,23 @@ import Web.Socket.WebSocket as WS
 import Web.Socket.Event.EventTypes as WSET
 import Web.Event.EventTarget as EET
 
+import Data.Argonaut.Core (stringify)
+import Data.Argonaut.Encode (class EncodeJson, encodeJson, (:=), (~>))
+import Data.Argonaut (jsonEmptyObject)
+
+newtype Message = Message
+  { text :: String
+  }
+
+instance encodeJsonMessage :: EncodeJson Message where
+  encodeJson (Message o) =
+    "text" := o.text ~> jsonEmptyObject
+
 -- conn <- WS.create "ws://echo.websocket.org" []
+
+z = stringify $ encodeJson (Message {text})
+  where
+    text = "hello"
 
 main :: Effect Unit
 main = do
@@ -17,7 +33,7 @@ main = do
     log "123"
   openListener <- EET.eventListener \ev -> do
     log "open"
-    WS.sendString socket "HELLO"
+    WS.sendString socket z
   EET.addEventListener WSET.onMessage listener false (WS.toEventTarget socket)
   EET.addEventListener WSET.onOpen openListener false (WS.toEventTarget socket)
   log "end of main"
