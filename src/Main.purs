3d1af8606bfd564870b215b77d5131108efdc8c8
Arunas Cesonis <karunce@gmail.com>
2018-07-19 12:47:08 +0100

    Split out Network

--- src/Main.purs
+++ src/Main.purs
@@ -3,62 +3,9 @@ module Main where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
-
-import Data.Foldable (for_)
-import Data.Either (Either(..), either, hush)
-import Data.Maybe (Maybe(..), fromMaybe)
-
-import Control.Monad.Except (runExcept)
-
-import Web.Socket.WebSocket as WS
-import Web.Socket.Event.EventTypes as WSET
-import Web.Socket.Event.MessageEvent as ME
-import Web.Event.EventTarget as EET
-
-import Data.Argonaut.Core (stringify)
-import Data.Argonaut.Parser (jsonParser)
-import Data.Argonaut.Encode (class EncodeJson, encodeJson, (:=), (~>))
-import Data.Argonaut.Decode (class DecodeJson, decodeJson, (.?))
-import Data.Argonaut (jsonEmptyObject)
-
-import Foreign (readString, unsafeToForeign)
-
-newtype Message = Message
-  { text :: String
-  }
-
-instance encodeJsonMessage :: EncodeJson Message where
-  encodeJson (Message o) =
-    "text" := o.text ~> jsonEmptyObject
-
-instance decodeJsonMessage :: DecodeJson Message where
-  decodeJson json = do
-    x <- decodeJson json
-    text <- x .? "text"
-    pure $ Message {text}
-
-z = stringify $ encodeJson (Message {text})
-  where
-    text = "hello"
-
-eventToString ev =
-  case ME.fromEvent ev of
-    Just msgEvent -> (fromMaybe "" <<< hush <<< runExcept <<< readString <<< unsafeToForeign <<< ME.data_) msgEvent
-    Nothing -> ""
-
-dec :: String -> Either String Message
-dec str = decodeJson =<< jsonParser str
-
-eventToMessage ev =
-  case (dec (eventToString ev)) of
-    Right (Message {text}) -> text
-    Left err -> err
+import Network (runNetwork)
 
 main :: Effect Unit
 main = do
-  socket <- WS.create "ws://localhost:7080" []
-  listener <- EET.eventListener (log <<< eventToMessage)
-  openListener <- EET.eventListener (const (WS.sendString socket z))
-  EET.addEventListener WSET.onMessage listener false (WS.toEventTarget socket)
-  EET.addEventListener WSET.onOpen openListener false (WS.toEventTarget socket)
+  runNetwork
   log "end of main"
---
+++ src/Network.purs
@@ -0,0 +1,67 @@
+module Network(runNetwork) where
+
+import Prelude
+import Effect (Effect)
+import Effect.Console (log)
+
+import Data.Either (Either(..), hush)
+import Data.Maybe (Maybe(..), fromMaybe)
+
+import Control.Monad.Except (runExcept)
+
+import Web.Socket.WebSocket as WS
+import Web.Socket.Event.EventTypes as WSET
+import Web.Socket.Event.MessageEvent as ME
+import Web.Event.EventTarget as EET
+import Web.Event.Event (Event)
+
+import Data.Argonaut.Core (stringify)
+import Data.Argonaut.Parser (jsonParser)
+import Data.Argonaut.Encode (class EncodeJson, encodeJson, (:=), (~>))
+import Data.Argonaut.Decode (class DecodeJson, decodeJson, (.?))
+import Data.Argonaut (jsonEmptyObject)
+
+import Foreign (readString, unsafeToForeign)
+
+newtype Message = Message
+  { text :: String
+  }
+
+instance encodeJsonMessage :: EncodeJson Message where
+  encodeJson (Message o) =
+    "text" := o.text ~> jsonEmptyObject
+
+instance decodeJsonMessage :: DecodeJson Message where
+  decodeJson json = do
+    x <- decodeJson json
+    text <- x .? "text"
+    pure $ Message {text}
+
+z :: String
+z = stringify $ encodeJson (Message {text})
+  where
+    text = "hello"
+
+eventToString :: Event -> String
+eventToString ev =
+  case ME.fromEvent ev of
+    Just msgEvent -> (fromMaybe "" <<< hush <<< runExcept <<< readString <<< unsafeToForeign <<< ME.data_) msgEvent
+    Nothing -> ""
+
+dec :: String -> Either String Message
+dec str = decodeJson =<< jsonParser str
+
+eventToMessage :: Event -> String
+eventToMessage ev =
+  case (dec (eventToString ev)) of
+    Right (Message {text}) -> text
+    Left err -> err
+
+runNetwork :: Effect Unit
+runNetwork = do
+  socket <- WS.create "ws://localhost:7080" []
+  listener <- EET.eventListener (log <<< eventToMessage)
+  openListener <- EET.eventListener (const (WS.sendString socket z))
+  EET.addEventListener WSET.onMessage listener false (WS.toEventTarget socket)
+  EET.addEventListener WSET.onOpen openListener false (WS.toEventTarget socket)
+  log "end of Network.run"
