ac857e34a41fbac21f598efd2da53c6addb83f4e
Arunas Cesonis <karunce@gmail.com>
2018-07-27 09:11:51 +0100

    Draw background

--- src/Main.purs
+++ src/Main.purs
@@ -7,25 +7,48 @@ import Effect.Console (log)
 import Data.Maybe (fromJust)
 
 import FRP.Event (Event, subscribe, fold)
-import FRP.Event.Keyboard (down, getKeyboard)
+import FRP.Event.Keyboard (down, getKeyboard, Keyboard)
 import FRP.Event.AnimationFrame (animationFrame)
 import FRP.Behavior.Keyboard (keys)
 import FRP.Behavior (sample_)
 
 import Graphics.Drawing (render)
-import Color (black)
+import Color (black, rgba)
 import Graphics.Drawing (Drawing, fillColor, filled, rectangle, text)
-import Graphics.Canvas (getCanvasElementById, getContext2D)
+import Graphics.Canvas (getCanvasElementById, getContext2D, getCanvasWidth, getCanvasHeight)
 
 import Partial.Unsafe (unsafePartial)
 
 import Network (runNetwork)
 
-draw _ = filled (fillColor black) (rectangle 0.0 0.0 20.0 20.0)
+red = rgba 255 0 0 1.0
 
-loop k x = show k
+type Vec2 =
+  { x :: Number
+  , y :: Number
+  }
 
-z kbd = fold loop (sample_ (keys kbd) animationFrame) "x"
+vec2 :: Number -> Number -> Vec2
+vec2 x y = {x, y}
+
+type State =
+  { stageSize :: Vec2
+  }
+
+initialState :: Vec2 -> State
+initialState stageSize =
+  { stageSize
+  }
+
+background {x: w, y: h} = filled (fillColor black) (rectangle 0.0 0.0 w h)
+
+draw state = background (state.stageSize)
+          <> filled (fillColor red) (rectangle 20.0 20.0 20.0 20.0)
+
+loop k state = state
+
+z :: Keyboard -> State -> Event State
+z kbd state = fold loop (sample_ (keys kbd) animationFrame) state
 
 main :: Effect Unit
 main = do
@@ -33,6 +56,9 @@ main = do
   mc <- getCanvasElementById "canvas"
   let canvas = unsafePartial (fromJust mc)
   ctx <- getContext2D canvas
+  w <- getCanvasWidth canvas
+  h <- getCanvasHeight canvas
+  state <- pure $ initialState (vec2 w h)
   kbd <- getKeyboard
-  _ <- subscribe (z kbd) (render ctx <<< draw)
+  _ <- subscribe (z kbd state) (render ctx <<< draw)
   log "end of main"
