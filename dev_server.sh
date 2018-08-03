2acba24b57c9b45093a51df27180b294e7fa8ea4
Arunas Cesonis <karunce@gmail.com>
2018-08-03 11:04:12 +0100

    Smarter shell scripts

--- dev_server.sh
+++ dev_server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
+cd $(dirname "$0")
 bower install
 exec pulp server -p 8080
--- test_server/run.sh
+++ test_server/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
+cd $(dirname "$0")
 npm i
 exec ./node_modules/.bin/ts-node index.ts
