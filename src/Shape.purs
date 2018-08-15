334182566815f34d4c8dd8d5af445065904a70ba
Arunas Cesonis <karunce@gmail.com>
2018-08-15 12:52:17 +0100

    Split out GenericJSON

--- src/Shape.purs
+++ src/Shape.purs
@@ -13,6 +13,7 @@ import Simple.JSON as JSON
 
 import Foreign as Foreign
 import Foreign (Foreign, readString)
+import GenericJSON (enumReadForeign)
 
 data Shape =
     Square Number
@@ -24,79 +25,11 @@ data Shape =
 
 derive instance genericShape :: GR.Generic Shape _
 
+instance showShape :: Show Shape where
+  show = genericShow
+
 instance shapeReadForeign :: JSON.ReadForeign Shape where
   readImpl = enumReadForeign
 
-enumReadForeign :: forall a rep
-  . GR.Generic a rep
-  => EnumReadForeign rep
-  => Foreign
-  -> Foreign.F a
-enumReadForeign f =
-  GR.to <$> enumReadForeignImpl f
-
-class EnumReadForeign rep where
-  enumReadForeignImpl :: Foreign -> Foreign.F rep
-
-class EnumReadForeignProduct rep where
-  enumReadForeignProductImpl :: Int -> Foreign -> Foreign.F rep
-
-contents :: String
-contents = "contents"
-
-tag :: String
-tag = "tag"
-
-instance sumEnumReadForeign ::
-  ( EnumReadForeign a
-  , EnumReadForeign b
-  ) => EnumReadForeign (GR.Sum a b) where
-  enumReadForeignImpl f
-    = GR.Inl <$> enumReadForeignImpl f
-    <|> GR.Inr <$> enumReadForeignImpl f
-
-instance argumentEnumReadForeign ::
-  ( JSON.ReadForeign a
-  ) => EnumReadForeign (GR.Argument a) where
-  enumReadForeignImpl f = GR.Argument <$> (JSON.readImpl =<< readProp contents f)
-
-instance noArgumentsEnumReadForeign :: EnumReadForeign GR.NoArguments where
-  enumReadForeignImpl _ = pure $ GR.NoArguments
-
-instance productEnumReadGenericProduct ::
-  ( EnumReadForeignProduct a
-  , EnumReadForeignProduct b
-  ) => EnumReadForeignProduct (GR.Product a b) where
-  enumReadForeignProductImpl i f = do
-      GR.Product
-      <$> enumReadForeignProductImpl i f
-      <*> enumReadForeignProductImpl (i + 1) f
-
-instance productEnumReadGenericArgument ::
-  ( JSON.ReadForeign a
-  ) => EnumReadForeignProduct (GR.Argument a) where
-  enumReadForeignProductImpl i f = do
-      GR.Argument <$> (JSON.readImpl =<< readIndex i f)
-
-instance productEnumReadForeign ::
-  ( EnumReadForeignProduct (GR.Product a b)
-  ) => EnumReadForeign (GR.Product a b) where
-  enumReadForeignImpl f = enumReadForeignProductImpl 0 =<< readProp contents f
-
-instance constructorEnumReadForeignPN ::
-  ( IsSymbol name
-  , EnumReadForeign a
-  ) => EnumReadForeign (GR.Constructor name a) where
-  enumReadForeignImpl f = do
-    tag <- readString =<< readProp tag f
-    if tag == name
-      then GR.Constructor <$> enumReadForeignImpl f
-      else throwError <<< pure <<< Foreign.ForeignError $ "BAM 3"
-    where
-      name = reflectSymbol (SProxy :: SProxy name)
-
 readShape :: String -> Either Foreign.MultipleErrors Shape
 readShape = JSON.readJSON
-
-instance showShape :: Show Shape where
-  show = genericShow
